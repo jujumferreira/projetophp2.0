d2f9e77bf445c2bbac0d7e4826104a478cf8a718
jujumferreira <juliadasilvanferreira@gmail.com>
2024-03-04 20:24:14 -0300

    exemplo 02 php

---
+++ exemplo02.php
@@ -0,0 +1,18 @@
+<?php 
+//declarando variáveis e inicializando
+$valor1 = 40.50;
+$valor2 = 20;
+$nome = "Etecia";
+$cidade = "são paulo";
+$flag = true;
+$statua = false;
+
+echo "$valor1 <br />";
+echo "$valor2 <br />";
+echo "$nome <br />";
+echo "$cidade <br />";
+echo "$flag <br />";
+echo "$status <br />";
+
+
+ ?>

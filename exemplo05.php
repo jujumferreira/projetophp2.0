1f8f705a24dc530447a760672f946bbc672acede
jujumferreira <juliadasilvanferreira@gmail.com>
2024-03-11 20:16:50 -0300

    exemplo 05

---
+++ exemplo05.php
@@ -0,0 +1,18 @@
+<?php 
+
+
+$nome = "Julinha";
+
+function exibir (){
+   $nome = "Josefina";
+
+   echo " Valor da variável dentro da função " . $nome;
+
+
+}
+
+   exibir();
+
+   echo " <br /> Valor da variável fora da função " . $nome;
+
+?>

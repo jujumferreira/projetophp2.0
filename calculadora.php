d6f357cd8cecb26e549d62842b87d38074a19858
jujumferreira <juliadasilvanferreira@gmail.com>
2024-03-20 22:09:56 -0300

    exemplo 06

---
+++ calculadora.php
@@ -0,0 +1,30 @@
+<?php 
+
+$num = 20.50;
+$num2 = 30.50;
+
+function soma ($num,$num2){
+  return ($num + $num2);
+}
+
+function subitracao ($num,$num2){
+  return ($num - $num2);
+
+}
+
+function divisao ($num,$num2){
+
+return ($num / $num2);
+
+}
+function multiplicacao ($num,$num2){
+
+return ($num * $num2);
+
+}
+
+
+
+
+
+ ?>
---
+++ exemplo06.php
@@ -0,0 +1,12 @@
+<?php 
+
+function calcular ($num,$num2){
+
+return ($num + $num2);
+
+}
+echo calcular (10,20);
+
+
+
+ ?>
